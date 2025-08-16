3fd4aed748ffde47572ce6bf87b137eca1a9dc5b
Jack <149383712+notnowJack@users.noreply.github.com>
2025-08-16 19:28:33 +0100

    Update test_8accountLockout.py

--- test_8accountLockout.py
+++ test_8accountLockout.py
@@ -6,7 +6,7 @@ def test_login_popup_appears(page: Page):
     page.goto("https://find.data.gov.scot/")
     page.get_by_text('Login').click()
 
-    page.get_by_role('textbox', name='input field for E.g. jane.doe').fill("abcdefgh@sharklasers.com")
+    page.get_by_role('textbox', name='input field for E.g. jane.doe').fill("a@a.com")
     page.get_by_role('textbox', name='input field for Enter password').fill("invalid_password")
 
     
@@ -16,4 +16,5 @@ def test_login_popup_appears(page: Page):
     not_bold = not ("font-weight" in error_element.evaluate("el => getComputedStyle(el).fontWeight") and int(error_element.evaluate("el => getComputedStyle(el).fontWeight")) >= 700)
     checkEmail = has_login_field_error_message_appeared(page, "You have made 5 incorrect login attempts and your account is locked", 0, not_bold)
         
+
     assert checkEmail is True
