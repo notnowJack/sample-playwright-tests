21b98ba2f6a2f14cceb6b358d43d0fb9b32badd5
Jack <149383712+notnowJack@users.noreply.github.com>
2025-08-16 19:28:24 +0100

    Update test_7counterIncrease_4.py - changed email

--- test_7counterIncrease_4.py
+++ test_7counterIncrease_4.py
@@ -7,7 +7,7 @@ def test_login_popup_appears(page: Page):
     page.goto("https://find.data.gov.scot/")
     page.get_by_text('Login').click()
 
-    page.get_by_role('textbox', name='input field for E.g. jane.doe').fill("abcdefgh@sharklasers.com")
+    page.get_by_role('textbox', name='input field for E.g. jane.doe').fill("a@a.com")
     page.get_by_role('textbox', name='input field for Enter password').fill("invalid_password")
     page.get_by_role('button', name='Log In').click()
 
@@ -16,5 +16,6 @@ def test_login_popup_appears(page: Page):
     checkEmail = has_login_field_error_message_appeared(page, "You have made 4 of 5 incorrect login attempt(s).", 0, not_bold)
 
     assert checkEmail is True
+
 
 
